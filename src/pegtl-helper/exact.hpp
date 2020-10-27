a0d5f3c6e1316bbbf5feb7e69e0fcc9e84bc2303
Mo Li <limo.iie4@gmail.com>
2020-10-27 20:38:25 +0800

    feat: Add exact rule to exactly match given str

---
+++ src/pegtl-helper/exact.hpp
@@ -0,0 +1,21 @@
+//
+// Created by ligen on 10/27/2020.
+//
+
+#ifndef FILE_REL_TYPE_EXACT_HPP
+#define FILE_REL_TYPE_EXACT_HPP
+
+#include <tao/pegtl.hpp>
+
+namespace TAO_PEGTL_NAMESPACE::contrib
+{
+
+    template<class Rule>
+    struct exact
+            : must< Rule, tao::pegtl::eof > {
+    };
+
+}
+
+
+#endif //FILE_REL_TYPE_EXACT_HPP
