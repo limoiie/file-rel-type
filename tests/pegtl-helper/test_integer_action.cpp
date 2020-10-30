7b6baea6ad307f06e2c089f589cac43acd958011
Mo Li <limo.iie4@gmail.com>
2020-10-30 19:57:32 +0800

    test: Refactor test and add test-cases for xxx_action< xxxInteger >

--- tests/pegtl-helper/test_integer_action.cpp
+++ tests/pegtl-helper/test_integer_action.cpp
@@ -18,21 +18,27 @@ using namespace tao::pegtl::helper::integer::action;
 
 #define PAIR(NUM) { #NUM, NUM }
 
-TEST(TestIntegerAction, test_to_decimal_unsigned_decimal) { // NOLINT(cert-err58-cpp)
-    std::cout << "Testing test_to_decimal_unsigned_decimal ..." << std::endl;
-    auto cases = std::list< std::pair< std::string, int>>{
-            PAIR(123),
-    };
-
-    for (auto const &pair : cases) {
+template< class Int, class Rule >
+void test_pattern(std::list<std::pair<std::string, Int>> const& cases) {
+    for (auto const& pair : cases) {
         std::cout << "  Case: " << pair.first << std::endl;
 
         memory_input in(pair.first, __FUNCTION__);
-        state_to_decimal< unsigned > st;
+        state_to_decimal< Int > st;
 
-        parse< unsigned_decimal, trait_action_to_decimal< unsigned >::action_to_decimal >(in, st);
+        parse< Rule, trait_action_to_decimal< Int >::template action_to_decimal >(in, st);
         ASSERT_EQ(st.val, pair.second);
     }
+
+}
+
+TEST(TestIntegerAction, test_to_decimal_unsigned_decimal) { // NOLINT(cert-err58-cpp)
+    std::cout << "Testing test_to_decimal_unsigned_decimal ..." << std::endl;
+    auto cases = std::list< std::pair< std::string, unsigned>>{
+            PAIR(123),
+    };
+
+    test_pattern< unsigned, unsigned_decimal >(cases);
 }
 
 TEST(TestIntegerAction, test_to_decimal_signed_decimal) { // NOLINT(cert-err58-cpp)
@@ -41,15 +47,7 @@ TEST(TestIntegerAction, test_to_decimal_signed_decimal) { // NOLINT(cert-err58-c
             PAIR(-123)
     };
 
-    for (auto const &pair : cases) {
-        std::cout << "  Case: " << pair.first << std::endl;
-
-        memory_input in(pair.first, __FUNCTION__);
-        state_to_decimal< signed > st;
-
-        parse< signed_decimal, trait_action_to_decimal< signed >::action_to_decimal >(in, st);
-        ASSERT_EQ(st.val, pair.second);
-    }
+    test_pattern< signed, signed_decimal >(cases);
 }
 
 TEST(TestIntegerAction, test_to_decimal_unsigned_0x_hex) { // NOLINT(cert-err58-cpp)
@@ -62,15 +60,7 @@ TEST(TestIntegerAction, test_to_decimal_unsigned_0x_hex) { // NOLINT(cert-err58-
             PAIR(0xDEADBEEF),
     };
 
-    for (auto const &pair : cases) {
-        std::cout << "  Case: " << pair.first << std::endl;
-
-        memory_input in(pair.first, __FUNCTION__);
-        state_to_decimal< unsigned > st;
-
-        parse< unsigned_0x_hex, trait_action_to_decimal< unsigned >::action_to_decimal >(in, st);
-        ASSERT_EQ(st.val, pair.second);
-    }
+    test_pattern< unsigned, unsigned_0x_hex >(cases);
 }
 
 TEST(TestIntegerAction, test_decimal_signed_0x_hex) { // NOLINT(cert-err58-cpp)
@@ -85,13 +75,31 @@ TEST(TestIntegerAction, test_decimal_signed_0x_hex) { // NOLINT(cert-err58-cpp)
             PAIR(-0xfcd123),
     };
 
-    for (auto const &pair : cases) {
-        std::cout << "  Case: " << pair.first << std::endl;
+    test_pattern< signed, signed_0x_hex >(cases);
+}
 
-        memory_input in(pair.first, __FUNCTION__);
-        state_to_decimal< signed > st;
+TEST(TestIntegerAction, test_decimal_unsigned_integer) { // NOLINT(cert-err58-cpp)
+    std::cout << "Testing test_decimal_unsigned_integer ..." << std::endl;
+    auto cases = std::list<std::pair<std::string , unsigned>>{
+            PAIR(0x123),
+            PAIR(0xDEADBEEF),
+            PAIR(123),
+            PAIR(1023)
+    };
 
-        parse< signed_0x_hex, trait_action_to_decimal< signed >::action_to_decimal >(in, st);
-        ASSERT_EQ(st.val, pair.second);
-    }
+    test_pattern< unsigned, unsigned_integer >(cases);
+}
+
+TEST(TestIntegerAction, test_decimal_signed_integer) { // NOLINT(cert-err58-cpp)
+    std::cout << "Testing test_decimal_signed_integer ..." << std::endl;
+    auto cases = std::list<std::pair<std::string , signed>>{
+            PAIR(+0),
+            PAIR(-123),
+            PAIR(+123098123),
+            PAIR(0x123),
+            PAIR(-0x123),
+            PAIR(+0x123),
+    };
+
+    test_pattern< signed, signed_integer >(cases);
 }
