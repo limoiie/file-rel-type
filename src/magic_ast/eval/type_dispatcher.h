d37cea4fdc26743e739d5e1d8720852f80793c4b
Mo Li <limo.iie4@gmail.com>
2020-11-20 16:38:14 +0800

    fix: Rename tmpl func to distinguish from a normal one with the same name
    Though the normal function is declared behind the template one, there still is possibility that the template one can see the normal function, where the bug fixed in this commit is, the tmpl func makes a wrong call to the normal one instead of itself. It is because the instantiation of template function always delay.

--- src/magic_ast/eval/type_dispatcher.h
+++ src/magic_ast/eval/type_dispatcher.h
@@ -55,14 +55,14 @@ namespace internal
     }
 
     template< template< std::size_t... > class Fn, std::size_t C, class... Ps, std::size_t... To >
-    auto dispatch_by_sizes(std::vector< std::size_t > const &ss, ids< To... > const &, Ps &&... ps) {
+    auto dispatch_by_sizes_(std::vector< std::size_t > const &ss, ids< To... > &&, Ps &&... ps) {
         if constexpr (C > 0) {
             constexpr auto D = C - 1;
             switch (ss[D]) {
-                case 1: return dispatch_by_sizes< Fn, D >(ss, ids< 1, To... >(), std::forward< Ps >(ps)...);
-                case 2: return dispatch_by_sizes< Fn, D >(ss, ids< 2, To... >(), std::forward< Ps >(ps)...);
-                case 4: return dispatch_by_sizes< Fn, D >(ss, ids< 4, To... >(), std::forward< Ps >(ps)...);
-                case 8: return dispatch_by_sizes< Fn, D >(ss, ids< 8, To... >(), std::forward< Ps >(ps)...);
+                case 1: return dispatch_by_sizes_< Fn, D >(ss, ids< 1, To... >(), std::forward< Ps >(ps)...);
+                case 2: return dispatch_by_sizes_< Fn, D >(ss, ids< 2, To... >(), std::forward< Ps >(ps)...);
+                case 4: return dispatch_by_sizes_< Fn, D >(ss, ids< 4, To... >(), std::forward< Ps >(ps)...);
+                case 8: return dispatch_by_sizes_< Fn, D >(ss, ids< 8, To... >(), std::forward< Ps >(ps)...);
             }
             throw std::logic_error("Failed to dispatch: not supported type size!");
         } else {
@@ -87,7 +87,7 @@ auto dispatch_by_2sizes_ex(std::size_t const from, std::size_t const to, Ps &&..
 template< template< std::size_t ... > class Fn, std::size_t C, class ...Ps >
 auto dispatch_by_sizes(std::vector< std::size_t > const &ss, Ps &&...ps) {
     assert(ss.size() == C);
-    return internal::dispatch_by_sizes< Fn, C >(ss, ids<>(), std::forward< Ps >(ps)...);
+    return internal::dispatch_by_sizes_< Fn, C >(ss, ids<>(), std::forward< Ps >(ps)...);
 }
 
 /**
@@ -98,7 +98,7 @@ template< class... Ss >
 struct dispatcher_by_sizes {
     template< template< std::size_t ... > class Fn, class ...Ps >
     static auto dispatch(Ss ...s, Ps &&...ps) {
-        return internal::dispatch_by_sizes< Fn, sizeof...(Ss) >({s...}, ids<>(), std::forward< Ps >(ps)...);
+        return internal::dispatch_by_sizes_< Fn, sizeof...(Ss) >({s...}, ids<>(), std::forward< Ps >(ps)...);
     }
 
 };
